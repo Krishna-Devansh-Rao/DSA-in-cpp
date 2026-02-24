076d880b72d5266334e0a83fed617daf24c72927
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-02-24 18:29:45 +0530

    Add initial C++ program for DSA journey

---
+++ 1_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "Welcome to the Journey of DSA in C++ " << "\n" ;
+cout << "Journey is started and it will not stop untill i reach somewhere" ;
+
+}
