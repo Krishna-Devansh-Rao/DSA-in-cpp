68bef3a94a57a1829214ed9ba3278067a1144692
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-02-24 18:15:17 +0530

    Add Bitwise operations examples in Bitwise_7.cpp

---
+++ Bitwise_7.cpp
@@ -0,0 +1,47 @@
+// Bitwise operator
+// 1) Bitwise & ( AND )
+// 2) Bitwise | ( OR )
+// 3) Bitwise ^ ( XOR )
+// 4) Bitwise << ( Left Shift)
+// 5) Bitwise >> (Right shift)
+
+
+#include <iostream>
+using namespace std;
+
+
+// int main(){
+//     cout << bitAnd(4,8);
+// }
+
+// AND
+// int main(){
+//     int a = 4, b = 8 ;
+//     cout << (a & b) << endl;
+//     return 0;
+// }
+
+// OR
+// int main(){
+//     int a = 4, b = 8 ;
+//     cout << (a | b) << endl;
+//     return 0;
+// }
+
+// XOR 
+// int main(){
+//     int a = 4, b = 8 ;
+//     cout << (a ^ b) << endl;
+//     return 0;
+// }
+
+// Left Shift 
+// int main(){
+//     cout << ( 10 << 2 );
+// }
+
+// Right Shift 
+// int main(){
+//     cout << ( 8 >> 2 );
+// }
+
