79286e58829ef54a3d85bdeca62f18b148e720ad
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-03-02 23:55:16 +0530

    Find smallest and largest numbers in an array

--- 8_Array.cpp
+++ 8_Array.cpp
@@ -27,3 +27,30 @@ for(int i = 0; i < 5; i++){
     return 0 ;
 }
 
+
+// to find out the smallest number 
+// int main(){
+//     int num[6] = {5,15,22,1,-15,24} ;
+//     int size = 6;
+//     int smallest = INT_MAX;
+//     for(int i = 0 ; i < size ; i++ ){
+//         if(num[i] < smallest){
+//             smallest = num[i];
+//         }
+//     }
+//     cout << "smallest is : " << smallest ;
+// }
+
+int main(){
+    int num[6] = {5,15,22,1,-15,24} ;
+    int size = 6;
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+    for(int i = 0 ; i < size ; i++ ){
+        smallest = min(num[i],smallest);
+        largest= max(num[i],largest);
+    }
+    cout << "smallest is : " << smallest << "\n" ;
+    cout << "largest is : " << largest ;
+}    
+
